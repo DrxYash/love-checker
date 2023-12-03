4f38a0bc5d8dfa1eaf13f4434e7af8201a1116f0
Devil-Yash <86905380+DrxYash@users.noreply.github.com>
2023-12-03 12:53:18 +0530

    Add files via upload

---
+++ love_checker.py
@@ -0,0 +1,33 @@
+while True:
+ m = str(input("Enter Your Love.: "))
+
+ if m == "mom" :
+    print("Very Good")
+ elif m == "dad":
+    print("Very Good")
+ elif m == "study":
+    print("Very Good")
+ elif m == "mother":
+    print("Very Good")
+ elif m == "father":
+    print("Very Good")
+ elif m == "brother":
+    print("Very Good")
+ elif m == "sister":
+    print("Very Good")
+ elif m == "grandmother":
+    print("Very Good")
+ elif m == "grandfather":
+    print("Very Good")
+ elif m == "grandparents":
+    print("Very Good")
+ elif m == "parents":
+    print("Very Good")
+ elif m == "chut":
+    for m in range(1,2):
+      print("Saala e t chut chatna ba")
+ else:
+    for i in range(1,11):
+       print("Bhaag Madharchod")
+   #  while True:
+   #  print("Bhaag Madharchod, " * 3,"Baag Na...... Madharchod")
